c1d9afa4425138932f5c49023537c7a93ab5f9d6
gerryfletch <1997gerry@gmail.com>
2019-07-07 14:53:34 +0100

    Updated project to JUnit5.

--- src/test/java/com/two/authentication/AuthenticationApplicationTests.java
+++ src/test/java/com/two/authentication/AuthenticationApplicationTests.java
@@ -1,16 +1,16 @@
 package com.two.authentication;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class AuthenticationApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
