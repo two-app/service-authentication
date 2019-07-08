d74b3dbb829757e3fef492194e5304b0a12ccbf3
gerryfletch <1997gerry@gmail.com>
2019-07-08 22:37:06 +0100

    Created bad request exception.

---
+++ src/main/java/com/two/authentication/exceptions/BadRequestException.java
@@ -0,0 +1,12 @@
+package com.two.authentication.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class BadRequestException extends ResponseStatusException {
+
+    public BadRequestException(String reason) {
+        super(HttpStatus.BAD_REQUEST, reason);
+    }
+
+}
