658ab7c8e9dfa4cec550922c34a031c77ad6d45a
gerryfletch <1997gerry@gmail.com>
2019-07-08 22:49:06 +0100

    Modified Tokens value to have separate constructors instead of null arguments.

--- src/main/java/com/two/authentication/tokens/Tokens.java
+++ src/main/java/com/two/authentication/tokens/Tokens.java
@@ -1,12 +1,24 @@
 package com.two.authentication.tokens;
 
-import lombok.Value;
+import lombok.Getter;
 
-@Value
+@Getter
 public class Tokens {
 
     private final RefreshToken refreshToken;
     private final AccessToken accessToken;
     private final ConnectToken connectToken;
+
+    Tokens(RefreshToken refreshToken, AccessToken accessToken) {
+        this.refreshToken = refreshToken;
+        this.accessToken = accessToken;
+        this.connectToken = null;
+    }
+
+    Tokens(RefreshToken refreshToken, ConnectToken connectToken) {
+        this.refreshToken = refreshToken;
+        this.accessToken = null;
+        this.connectToken = connectToken;
+    }
 
 }
